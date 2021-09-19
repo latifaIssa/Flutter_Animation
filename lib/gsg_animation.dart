0adee515cf361567f7d35d1ee7efc02f965d2058
latifaIssa <latifa.masri1998@gmail.com>
2021-09-19 17:56:01 +0300

    using slide transition

--- lib/gsg_animation.dart
+++ lib/gsg_animation.dart
@@ -11,37 +11,51 @@ class GsgAnimation extends StatefulWidget {
 class _GsgAnimationState extends State<GsgAnimation>
     with SingleTickerProviderStateMixin {
   AnimationController animationController;
+  // AnimationController animationController2;
+
   Animation animation;
+  // Animation animation2;
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      duration: Duration(seconds: 3),
-      vsync: this,
-    );
-    final curvedAnimation = CurvedAnimation(
-      parent: animationController,
-      curve: Curves.easeIn,
-      reverseCurve: Curves.easeInCirc,
-    );
-    animation = Tween<double>(
-      begin: 0,
-      end: 2 * math.pi,
-    ).chain(CurveTween(curve: Curves.bounceInOut)).animate(animationController)
-      ..addListener(() {
-        setState(() {});
+    // animationController = AnimationController(
+    //   duration: Duration(seconds: 3),
+    //   vsync: this,
+    // );
+    // // final curvedAnimation = CurvedAnimation(
+    // //   parent: animationController,
+    // //   curve: Curves.easeIn,
+    // //   reverseCurve: Curves.easeInCirc,
+    // // );
+    // animation = Tween<double>(
+    //   begin: 0,
+    //   end: 2 * math.pi,
+    // ).chain(CurveTween(curve: Curves.bounceInOut)).animate(animationController)
+    //   ..addListener(() {
+    //     setState(() {});
 
-        //to show the animation status
-      })
-      ..addStatusListener((status) {
-        //completed: finished
-        //dismiss: return to 0
-        if (status == AnimationStatus.completed) {
-          animationController.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          animationController.forward();
-        }
-      });
+    //     //to show the animation status
+    //   })
+    //   ..addStatusListener((status) {
+    //     //completed: finished
+    //     //dismiss: return to 0
+    //     if (status == AnimationStatus.completed) {
+    //       animationController.reverse();
+    //     } else if (status == AnimationStatus.dismissed) {
+    //       animationController.forward();
+    //     }
+    //   });
+    // animationController.forward();
+
+    /////for Transiton//////
+    animationController = AnimationController(
+      duration: Duration(seconds: 99),
+      vsync: this,
+    )..repeat(reverse: true);
+    animation = Tween<Offset>(
+      begin: Offset.zero,
+      end: Offset(0, -1),
+    ).animate(animationController);
     animationController.forward();
   }
 
@@ -56,8 +70,24 @@ class _GsgAnimationState extends State<GsgAnimation>
       //     ),
       //   ),
       // ),
-      body: Transform.scale(
-        scale: animation.value,
+      // body: Transform.scale(
+      //   scale: animation.value,
+      //   child: Center(
+      //     child: Container(
+      //       child: FlutterLogo(),
+      //     ),
+      //   ),
+      // ),
+      // body: Transform.translate(
+      //   offset: Offset(0, -100), // take just one point so the setState don't updated
+      //   child: Center(
+      //     child: Container(
+      //       child: FlutterLogo(),
+      //     ),
+      //   ),
+      // ),
+      body: SlideTransition(
+        position: animation,
         child: Center(
           child: Container(
             child: FlutterLogo(),
@@ -65,5 +95,12 @@ class _GsgAnimationState extends State<GsgAnimation>
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    // animationController2.dispose();
+    super.dispose();
   }
 }
